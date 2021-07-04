11927eb4b2005ddde355dc9f2eac48ceed08aa3a
DoğukanKazan <kazand25@gmail.com>
2021-07-05 00:56:23 +0300

    Asp.NetMvcMssqlFootballManagerDemo
    WebProjectDemo

---
+++ FootballTeamProject/Controllers/TeamController.cs
@@ -0,0 +1,69 @@
+﻿using FootballTeamProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+namespace FootballTeamProject.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var t = c.Teams.ToList();
+            return View(t);
+        }
+
+        [HttpGet]
+        public ActionResult AddNewTeam()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult AddNewTeam(Team t)
+        {
+            c.Teams.Add(t);
+            c.SaveChanges();
+            return View();
+
+        }
+
+        public ActionResult DeleteTeam(int id)
+        {
+            var x = c.Teams.Find(id);
+            c.Teams.Remove(x);
+            c.SaveChanges();
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateTeam(int id)
+        {
+            var x = c.Teams.Find(id);
+            return View("UpdateTeam",x);
+
+        }
+        [HttpPost]
+        public ActionResult UpdateTeam(Team t)
+        {
+            var x = c.Teams.Find(t.id);
+            x.Name = t.Name;
+            x.Since = t.Since;
+            x.Country = t.Country;
+            x.Budget = t.Budget;
+            x.Salary = t.Salary;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Me()
+        {
+            return View("");
+        }
+
+    }
+}
---
+++ FootballTeamProject/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FootballTeamProject.Models
+{
+    public class Team
+    {
+
+        [Key]
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Since { get; set; }
+        public string Country { get; set; }
+        public int Budget { get; set; }
+        public int Salary { get; set; }
+    }
+}
